5ebe6b601fc43f9996c75a6001812295d9118c25
Sujanani <ansujanani@hotmail.com>
2018-10-03 15:33:00 +0530

    Added bucketlist class, renamed User to user

---
+++ src/models/bucketlist.py
@@ -0,0 +1,26 @@
+class Bucketlist:
+	name = ''
+	description = ''
+	items = []
+	complete = False
+	active = False
+
+	def __init__(self):
+		print('Bucketlist object created')
+
+	def create(self, str_p1, str_p2, list_p3):
+		return None
+
+	def index(self):
+		return None
+
+	def view(self, int_p1):
+		return None
+
+	def update(self, str_p1, str_p2, list_p3):
+		return None
+
+	def delete(int_p1):
+		return None
+
+
