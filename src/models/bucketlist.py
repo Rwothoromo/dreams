bb9a1e17ed5bc15ed6b9738625e0872370a73e8f
Sujanani <ansujanani@hotmail.com>
2018-10-03 18:59:19 +0530

    Modified init of bucketlist

--- src/models/bucketlist.py
+++ src/models/bucketlist.py
@@ -5,8 +5,12 @@ class Bucketlist:
 	complete = False
 	active = False
 
-	def __init__(self):
-		print('Bucketlist object created')
+	def __init__(self, name, description, items, complete=False, active=False):
+		self.name = name
+		self.description = description
+		self.items = items
+		self.complete = complete
+		self.active = active
 
 	def create(self, str_p1, str_p2, list_p3):
 		return None
